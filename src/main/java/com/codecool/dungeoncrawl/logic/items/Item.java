995fd559027871dc4b4420a43b5f312c5cdf5a38
trbogi <trencsenyi.bogi@gmail.com>
2022-03-08 09:39:45 +0100

    Add item field and its getter and setter to Cell class

--- src/main/java/com/codecool/dungeoncrawl/logic/items/Item.java
+++ src/main/java/com/codecool/dungeoncrawl/logic/items/Item.java
@@ -1,8 +1,10 @@
 package com.codecool.dungeoncrawl.logic.items;
 
+import com.codecool.dungeoncrawl.logic.Cell;
 import com.codecool.dungeoncrawl.logic.Drawable;
 
 public abstract class Item implements Drawable {
+    private Cell cell;
     @Override
     public String getTileName() {
         return "item";
