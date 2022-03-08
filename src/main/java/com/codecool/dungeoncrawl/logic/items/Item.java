ebe0e393ed1fd6d33e07491d10bbd3319a3082b7
trbogi <trencsenyi.bogi@gmail.com>
2022-03-08 09:34:45 +0100

    Make Item class abstract

--- src/main/java/com/codecool/dungeoncrawl/logic/items/Item.java
+++ src/main/java/com/codecool/dungeoncrawl/logic/items/Item.java
@@ -2,7 +2,7 @@ package com.codecool.dungeoncrawl.logic.items;
 
 import com.codecool.dungeoncrawl.logic.Drawable;
 
-public class Item implements Drawable {
+public abstract class Item implements Drawable {
     @Override
     public String getTileName() {
         return "item";
