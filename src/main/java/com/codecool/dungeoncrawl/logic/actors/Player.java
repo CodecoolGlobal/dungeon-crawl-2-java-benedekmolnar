f753c412d79685234e4e58f4fae4c42950a061c8
trbogi <trencsenyi.bogi@gmail.com>
2022-03-08 12:45:45 +0100

    Add inventory hashmap to Player

--- src/main/java/com/codecool/dungeoncrawl/logic/actors/Player.java
+++ src/main/java/com/codecool/dungeoncrawl/logic/actors/Player.java
@@ -1,10 +1,20 @@
 package com.codecool.dungeoncrawl.logic.actors;
 
 import com.codecool.dungeoncrawl.logic.Cell;
+import com.sun.javafx.collections.MappingChange;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class Player extends Actor {
+    private Map<String, Integer> inventory = new HashMap<>();
+
     public Player(Cell cell) {
         super(cell);
+    }
+
+    public Map<String, Integer> getInventory(){
+        return inventory;
     }
 
     public String getTileName() {
