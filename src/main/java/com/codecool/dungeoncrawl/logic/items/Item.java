455dee2f5c78c0ad12ceb1e21ab3a22fb71764ac
trbogi <trencsenyi.bogi@gmail.com>
2022-03-08 09:33:30 +0100

    Create Item class which implements Drawable

---
+++ src/main/java/com/codecool/dungeoncrawl/logic/items/Item.java
@@ -0,0 +1,10 @@
+package com.codecool.dungeoncrawl.logic.items;
+
+import com.codecool.dungeoncrawl.logic.Drawable;
+
+public class Item implements Drawable {
+    @Override
+    public String getTileName() {
+        return "item";
+    }
+}
