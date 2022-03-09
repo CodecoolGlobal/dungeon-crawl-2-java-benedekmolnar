23d8f6b16898f48822a37b9d718a48f72a2082c6
trbogi <trencsenyi.bogi@gmail.com>
2022-03-09 13:50:43 +0100

    Change inventory map's key type from String to Item

--- src/main/java/com/codecool/dungeoncrawl/logic/actors/Player.java
+++ src/main/java/com/codecool/dungeoncrawl/logic/actors/Player.java
@@ -10,7 +10,7 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class Player extends Actor {
-    private Map<String, Integer> inventory = new HashMap<>();
+    private Map<Item, Integer> inventory = new HashMap<>();
 
     public Player(Cell cell) {
         super(cell);
@@ -22,12 +22,12 @@ public class Player extends Actor {
         if (itemToPickUp instanceof Cheese){
             changeHealth(2);
         }else{
-            this.addItemToInventory(itemToPickUp.getTileName());
+            this.addItemToInventory(itemToPickUp);
         }
         currentCell.setItem(null);
     }
 
-    public void addItemToInventory(String item){
+    public void addItemToInventory(Item item){
         if (inventory.get(item) != null){
             inventory.put(item, inventory.get(item) + 1);
         }else{
@@ -41,8 +41,8 @@ public class Player extends Actor {
 
     public String inventoryToString() {
         StringBuilder inventoryAsString = new StringBuilder();
-        for (String key : inventory.keySet()) {
-            inventoryAsString.append(key + "=" + inventory.get(key) + "   ");
+        for (Item key : inventory.keySet()) {
+            inventoryAsString.append(key.getTileName() + "=" + inventory.get(key) + "   ");
             inventoryAsString.append("\n");
         }
         return inventoryAsString.toString();
