b942e06b6f1886da9e754f6b5279bc678439e20c
trbogi <trencsenyi.bogi@gmail.com>
2022-03-23 14:14:47 +0100

    Add isPlayerNameInDatabase to GameStateDaoJdbc

--- src/main/java/com/codecool/dungeoncrawl/dao/GameStateDaoJdbc.java
+++ src/main/java/com/codecool/dungeoncrawl/dao/GameStateDaoJdbc.java
@@ -3,6 +3,10 @@ package com.codecool.dungeoncrawl.dao;
 import com.codecool.dungeoncrawl.model.GameState;
 
 import javax.sql.DataSource;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.List;
 
 public class GameStateDaoJdbc implements GameStateDao {
@@ -30,5 +34,17 @@ public class GameStateDaoJdbc implements GameStateDao {
     @Override
     public List<GameState> getAll() {
         return null;
+    }
+
+    public boolean isPlayerNameInDatabase(String playerName){
+        try (Connection conn = dataSource.getConnection()) {
+            String sql = "SELECT 1 FROM game_state WHERE player_name = ?";
+            PreparedStatement st = conn.prepareStatement(sql);
+            st.setString(1, playerName);
+            ResultSet rs = st.executeQuery();
+            return rs.next();
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
