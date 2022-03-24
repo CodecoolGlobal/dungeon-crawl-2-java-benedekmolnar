d7abb09888993fd869847c82e85b8b1c62e45c93
trbogi <trencsenyi.bogi@gmail.com>
2022-03-24 13:01:36 +0100

    Complete ActorsModel

--- src/main/java/com/codecool/dungeoncrawl/model/ActorsModel.java
+++ src/main/java/com/codecool/dungeoncrawl/model/ActorsModel.java
@@ -6,24 +6,28 @@ public class ActorsModel extends BaseModel{
     private int x;
     private int y;
     private int hp;
-    private String text;
+    private String direction;
+    private String data;
+    private int cooldown_timer;
 
 
-    public ActorsModel(int gameStateId, String type, int x, int y, int hp, String text) {
+    public ActorsModel(int gameStateId, String type, int x, int y, int hp, String direction, String data, int cooldown_timer) {
         this.gameStateId = gameStateId;
         this.type = type;
         this.x = x;
         this.y = y;
         this.hp = hp;
-        this.text = text;
+        this.direction = direction;
+        this.data = data;
+        this.cooldown_timer = cooldown_timer;
     }
 
-    public String getText() {
-        return text;
+    public String getDirection() {
+        return direction;
     }
 
-    public void setText(String text) {
-        this.text = text;
+    public void setDirection(String direction) {
+        this.direction = direction;
     }
 
     public int getHp() {
@@ -64,5 +68,21 @@ public class ActorsModel extends BaseModel{
 
     public void setGameStateId(int gameStateId) {
         this.gameStateId = gameStateId;
+    }
+
+    public int getCooldown_timer() {
+        return cooldown_timer;
+    }
+
+    public void setCooldown_timer(int cooldown_timer) {
+        this.cooldown_timer = cooldown_timer;
+    }
+
+    public String getData() {
+        return data;
+    }
+
+    public void setData(String data) {
+        this.data = data;
     }
 }
