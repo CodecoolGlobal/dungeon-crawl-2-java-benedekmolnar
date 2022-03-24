e35fbca1f8b772a65edf41e5a03022b7fcd6a4b7
Guszti9 <tusi0531@gmail.com>
2022-03-24 14:10:58 +0100

    Create ItemModel

--- src/main/java/com/codecool/dungeoncrawl/logic/items/Item.java
+++ src/main/java/com/codecool/dungeoncrawl/logic/items/Item.java
@@ -6,8 +6,18 @@ import com.codecool.dungeoncrawl.logic.Drawable;
 public abstract class Item implements Drawable {
     private Cell cell;
 
+    public Cell getCell() {
+        return cell;
+    }
+
+    public void setCell(Cell cell) {
+        this.cell = cell;
+    }
+
     public Item(Cell cell) {
         this.cell = cell;
         this.cell.setItem(this);
     }
+
+
 }
