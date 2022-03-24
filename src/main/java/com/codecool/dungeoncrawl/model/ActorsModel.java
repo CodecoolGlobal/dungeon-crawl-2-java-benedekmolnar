78f7032d5fe656706949a84df2b4e8dcd7905eab
trbogi <trencsenyi.bogi@gmail.com>
2022-03-24 12:55:38 +0100

    Implement ActorsModel

--- src/main/java/com/codecool/dungeoncrawl/model/ActorsModel.java
+++ src/main/java/com/codecool/dungeoncrawl/model/ActorsModel.java
@@ -1,4 +1,68 @@
 package com.codecool.dungeoncrawl.model;
 
-public class ActorsModel {
+public class ActorsModel extends BaseModel{
+    private int gameStateId;
+    private String type;
+    private int x;
+    private int y;
+    private int hp;
+    private String text;
+
+
+    public ActorsModel(int gameStateId, String type, int x, int y, int hp, String text) {
+        this.gameStateId = gameStateId;
+        this.type = type;
+        this.x = x;
+        this.y = y;
+        this.hp = hp;
+        this.text = text;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+
+    public int getHp() {
+        return hp;
+    }
+
+    public void setHp(int hp) {
+        this.hp = hp;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public void setY(int y) {
+        this.y = y;
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public void setX(int x) {
+        this.x = x;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public int getGameStateId() {
+        return gameStateId;
+    }
+
+    public void setGameStateId(int gameStateId) {
+        this.gameStateId = gameStateId;
+    }
 }
