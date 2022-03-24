3ebcc68c064ad7c3a834d47b0d0841c3f63357ac
trbogi <trencsenyi.bogi@gmail.com>
2022-03-24 12:42:13 +0100

    Implement add method in GameStateDaoJdbc

--- src/main/java/com/codecool/dungeoncrawl/dao/GameStateDaoJdbc.java
+++ src/main/java/com/codecool/dungeoncrawl/dao/GameStateDaoJdbc.java
@@ -3,10 +3,7 @@ package com.codecool.dungeoncrawl.dao;
 import com.codecool.dungeoncrawl.model.GameState;
 
 import javax.sql.DataSource;
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.SQLException;
+import java.sql.*;
 import java.util.List;
 
 public class GameStateDaoJdbc implements GameStateDao {
@@ -18,7 +15,20 @@ public class GameStateDaoJdbc implements GameStateDao {
 
     @Override
     public void add(GameState state) {
+        try(Connection conn = dataSource.getConnection()) {
+            String sql = "INSERT INTO game_state (player_name, map, saved_at) VALUES (?, ?, ?)";
+            PreparedStatement st = conn.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS);
+            st.setString(1, state.getPlayerName());
+            st.setString(2, state.getCurrentMap());
+            st.setDate(3, state.getSavedAt());
+            st.executeUpdate();
+            ResultSet rs = st.getGeneratedKeys();
+            rs.next(); // Read next returned value - in this case the first one. See ResultSet docs for more explaination
+            state.setId(rs.getInt(1));
 
+        } catch (SQLException throwables) {
+            throw new RuntimeException("Error while adding new Author.", throwables);
+        }
     }
 
     @Override
